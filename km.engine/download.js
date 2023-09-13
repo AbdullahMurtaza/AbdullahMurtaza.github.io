00d9c8ba5a6058c88dc57ff63fbbf7a076a80bcb
Abdullah Murtaza <93546512+AbdullahMurtaza@users.noreply.github.com>
2023-09-13 07:36:25 +0500

    .

--- km.engine/download.js
+++ km.engine/download.js
@@ -1,40 +1,60 @@
 // download.js
 
-// Function to download selected pages of the PDF
-function downloadPages(pageNumbers) {
-  const fileInput = document.getElementById("pdfFile");
-  const file = fileInput.files[0];
+// Function to download selected pages as a new PDF
+function downloadSelectedPages(selectedPages) {
+  // Initialize a new PDF document
+  const pdfDoc = new PDFDocument();
 
-  if (file) {
-    const fileReader = new FileReader();
+  // Add each selected page to the new PDF
+  selectedPages.forEach(async (pageNumber) => {
+    // Load the page from the original PDF (modify as needed)
+    const originalPage = await loadPageFromOriginalPDF(pageNumber);
 
-    fileReader.onload = async function () {
-      const typedArray = new Uint8Array(this.result);
-      const pdf = await pdfjsLib.getDocument(typedArray).promise;
+    // Add the loaded page to the new PDF
+    pdfDoc.addPage(originalPage);
+  });
 
-      const pdfDoc = await pdfjsLib.getDocument({ data: typedArray }).promise;
-      const pdfPages = [];
+  // Create a writable stream to save the new PDF
+  const stream = pdfDoc.pipe(blobStream());
 
-      for (const pageNumber of pageNumbers) {
-        const page = await pdfDoc.getPage(pageNumber);
-        const pdfPage = await page.getData();
-        pdfPages.push(pdfPage);
-      }
+  // End the PDF creation process
+  pdfDoc.end();
 
-      const blob = new Blob(pdfPages, { type: "application/pdf" });
-      const url = URL.createObjectURL(blob);
+  // Trigger the download of the new PDF
+  stream.on("finish", () => {
+    const blob = stream.toBlob("application/pdf");
+    const url = URL.createObjectURL(blob);
 
-      // Create a temporary link element to trigger the download
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = "selected_pages.pdf";
-      link.style.display = "none";
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-      URL.revokeObjectURL(url);
-    };
+    // Create a download link and trigger a click event to initiate the download
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "selected_pages.pdf";
+    a.style.display = "none";
+    document.body.appendChild(a);
+    a.click();
 
-    fileReader.readAsArrayBuffer(file);
-  }
+    // Clean up
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  });
 }
+
+// Example function to load a page from the original PDF (replace with your implementation)
+async function loadPageFromOriginalPDF(pageNumber) {
+  // Replace this with code to load the specified page from the original PDF
+  // You might use pdf.js or any other library for this purpose
+  // Example using pdf.js:
+  const pdf = await pdfjsLib.getDocument({ url: 'original.pdf' }).promise;
+  const page = await pdf.getPage(pageNumber);
+  const viewport = page.getViewport({ scale: 1.0 });
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  canvas.width = viewport.width;
+  canvas.height = viewport.height;
+  await page.render({ canvasContext: context, viewport }).promise;
+  return pdfDocRef.embedPFB(new Uint8Array(buffer));
+}
+
+// Example usage
+const selectedPages = [1, 3, 5]; // Replace with the pages you want to download
+downloadSelectedPages(selectedPages);
